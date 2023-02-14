db62ebeb57135a2e7c51e00336d3393a9a400e34
Vitor Silva <vitor.azevedo009@gmail.com>
2023-02-14 10:35:21 -0300

    Fix - Theme: Fixing theme configuration colors

--- src/chakra/theme.tsx
+++ src/chakra/theme.tsx
@@ -3,20 +3,21 @@ import "@fontsource/open-sans/300.css"
 import "@fontsource/open-sans/400.css"
 import "@fontsource/open-sans/700.css"
 
-export const colors = {
+const colors = {
     brand: {
         100: '#FF3c00',
     },
-    fonts: {
-        body: "Open Sans, sans-serif"
-    },
-    styles: {
-        global: () => ({
-            body: {
-                bg: 'gray.200',
-            },
-        }),
-    },
 }
 
-export const theme = extendTheme({ colors })
+const fonts = {
+    body: "Open Sans, sans-serif"
+}
+
+const styles = {
+    global: () => ({
+        body: {
+            bg: 'gray.200',
+        },
+    }),
+}
+export const theme = extendTheme({ colors, fonts, styles })
