67b3df67229a5cd67c4f5ac491f4d93763fda731
Vitor Silva <vitor.azevedo009@gmail.com>
2023-02-22 14:57:55 -0300

    Modal auth with initial Recoil configuration

--- src/components/Navbar/RightContent/AuthButtons.tsx
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -5,8 +5,19 @@ import { Button } from '@chakra-ui/react'
 function AuthButtons() {
     return (
         <>
-            <Button>Log in</Button>
-            <Button>Sign Up</Button>
+            <Button
+                variant="outline"
+                height="28px"
+                display={{ base: "none", sm: "flex" }}
+                width={{ base: "70px", md: "110px" }}
+                mr={2}
+            >Log in</Button>
+            <Button
+                height="28px"
+                display={{ base: "none", sm: "flex" }}
+                width={{ base: "70px", md: "110px" }}
+                mr={2}
+            >Sign Up</Button>
         </>
     )
 }
