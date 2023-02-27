5ffc75403ed7a3109a5969e49998eb9f760ec655
Vitor Silva <vitor.azevedo009@gmail.com>
2023-02-27 11:33:07 -0300

    Finishing style Login component

--- src/components/Modal/Login.tsx
+++ src/components/Modal/Login.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Input, Button } from '@chakra-ui/react'
-import { useRecoilState } from 'recoil'
+import { Input, Button, Text, Flex } from '@chakra-ui/react'
+import { useRecoilState, useSetRecoilState } from 'recoil'
 import { authModalState } from '@/src/atoms/authModalAtom'
 
 type LoginProps = {
@@ -9,16 +9,18 @@ type LoginProps = {
 
 const Login: React.FC<LoginProps> = () => {
 
+    const setAuthModalState = useSetRecoilState(authModalState)
+
     const [loginForm, setLoginForm] = useRecoilState(authModalState)
 
-    const onSubmit = () => {};
+    const onSubmit = () => { };
 
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setLoginForm((prev) => ({
-                ...prev,
-                [event.target.name]: [event.target.value],
-            }))
-        };
+            ...prev,
+            [event.target.name]: [event.target.value],
+        }))
+    };
 
     return (
         <form onSubmit={onSubmit}>
@@ -30,18 +32,18 @@ const Login: React.FC<LoginProps> = () => {
                 mb={2}
                 onChange={onChange}
                 fontSize={10}
-                _placeholder={{color: 'gray.500'}}
+                _placeholder={{ color: 'gray.500' }}
                 _hover={{
-                        bg: "white",
-                        border: "1px solid",
-                        borderColor: "blue.500",
-                    }}
+                    bg: "white",
+                    border: "1px solid",
+                    borderColor: "blue.500",
+                }}
                 _focus={{
-                        outline: "none",
-                        bg: "white",
-                        border: "1px solid",
-                        borderColor: "blue.500"
-                    }}
+                    outline: "none",
+                    bg: "white",
+                    border: "1px solid",
+                    borderColor: "blue.500"
+                }}
             />
             <Input
                 required
@@ -51,20 +53,37 @@ const Login: React.FC<LoginProps> = () => {
                 mb={2}
                 onChange={onChange}
                 fontSize={10}
-                _placeholder={{color: 'gray.500'}}
+                _placeholder={{ color: 'gray.500' }}
                 _hover={{
-                        bg: "white",
-                        border: "1px solid",
-                        borderColor: "blue.500",
-                    }}
+                    bg: "white",
+                    border: "1px solid",
+                    borderColor: "blue.500",
+                }}
                 _focus={{
-                        outline: "none",
-                        bg: "white",
-                        border: "1px solid",
-                        borderColor: "blue.500"
-                    }}
+                    outline: "none",
+                    bg: "white",
+                    border: "1px solid",
+                    borderColor: "blue.500"
+                }}
             />
             <Button width="100%" height="36px" mt={2} mb={2} type="submit">Log in</Button>
+            <Flex
+                fontSize="9pt"
+                justifyContent="center">
+                <Text
+                    mr={1}
+                >New here?</Text>
+                <Text
+                    color="blue.500"
+                    fontWeight={700}
+                    cursor="pointer"
+                    onClick={() => {
+                        setAuthModalState((prev) => ({
+                            ...prev,
+                            view: "signup",
+                        }))
+                    }}>SIGN UP</Text>
+            </Flex>
         </form>
     )
 }
