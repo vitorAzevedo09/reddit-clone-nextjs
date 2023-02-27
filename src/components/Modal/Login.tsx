e4aad7b9e14ea81d3438398dc42ad0cc15ca6726
Vitor Silva <vitor.azevedo009@gmail.com>
2023-02-27 11:07:42 -0300

    Added onChange function Login inputs

--- src/components/Modal/Login.tsx
+++ src/components/Modal/Login.tsx
@@ -1,28 +1,42 @@
 import React from 'react'
 import { Input, Button } from '@chakra-ui/react'
+import { useRecoilState } from 'recoil'
+import { authModalState } from '@/src/atoms/authModalAtom'
 
 type LoginProps = {
 
 }
 
 const Login: React.FC<LoginProps> = () => {
+
+    const [loginForm, setLoginForm] = useRecoilState(authModalState)
+
+    const onSubmit = () => {};
+
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setLoginForm((prev) => ({
+                ...prev,
+                [event.target.name]: [event.target.value],
+            }))
+        };
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <Input
                 name="email"
                 placeholder="email"
                 type="email"
                 mb={2}
-                onChange={() => { }}
+                onChange={onChange}
             />
             <Input
                 name="password"
                 placeholder="password"
                 type="password"
                 mb={2}
-                onChange={() => { }}
+                onChange={onChange}
             />
-            <Button>Log in</Button>
+            <Button type="submit">Log in</Button>
         </form>
     )
 }
